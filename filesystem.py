be559bc758112af9069339b7ca2f11c447ca857e
emm312 <emmtbm55@gmail.com>
2022-02-22 13:05:30 +1100

    bug fixes and shiii. if you can please fix removing directories and files

--- filesystem.py
+++ filesystem.py
@@ -1,11 +1,8 @@
 class File:
-    def __init__(self, name, ext):
+    def __init__(self, name, ext, parentFolder):
         self.name = name
         self.ext = ext
-
-
-    def getFullName(self):
-        return self.name+"."+self.extension
+        self.parentFolder = ""
     def getParentFolder(self):
         return self.parentFolder
     #make a ToJson method
@@ -27,11 +24,26 @@ class Folder:
         self.files.append(file)
     def addFolder(self, folder):
         self.folders.append(folder)
-
+    def removeFolder(self, name):
+        for folder in self.folders:
+            if folder.name == name:
+                self.folders.remove(folder)
+                return
+            else:
+                print("Folder not found.")
+        
+    def removeFile(self, name):
+        for file in self.files:
+            if file.name == name:
+                self.files.remove(file)
+                return
+            else:
+                print("File not found.")
+    
     # make a toJson function
     def toJson(self):
         # remember to convert the files and folders to json
-        return {name : {
+        return {self.name : {
             "name": self.name,
             "parentFolder": self.parentFolder,
             "files": [file.toJson() for file in self.files],
--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ print("Python OS Loading!")
 import os
 import filesystem
 root_folder = filesystem.Folder("root", None)
-fresh_filesys = {
+fresh_filesys = {"root":{
     "name": "root",
     "parent": None,
     "files": [],
@@ -12,7 +12,7 @@ fresh_filesys = {
             "parent": "root",
             "files": [],
             "folders": []}}]
-}
+}}
 
 import json
 
@@ -33,9 +33,24 @@ with open('filesystem.json', 'r') as outfile:
 def updateFilesystem():
     with open('filesystem.json', 'w') as out:
         json.dump(filesys, out, indent=4)
+    convertFilesystemToObjects()
 
 
+def convertFilesystemToObjects():
+    for folder in filesys["root"]["folders"]:
+        for folder_name in folder:
+            folder_obj = filesystem.Folder(folder_name, "root")
+            for file in folder[folder_name]["files"]:
+                file_obj = filesystem.File(file["name"], file["ext"])
+                folder_obj.addFile(file_obj)
+            for folder_name in folder[folder_name]["folders"]:
+                folder_obj.addFolder(folder_name)
+            root_folder.addFolder(folder_obj)
+    for file in filesys["root"]["files"]:
+        file_obj = filesystem.File(file["name"], file["ext"])
+        root_folder.addFile(file_obj)
 
+    
 isOn = True
 print("Done!")
 
@@ -59,11 +74,19 @@ while True:
         print("shutdown: turns off os")
         print("reboot: reboots the os")
         print("cls: clears screen")
+        print("mkdir: makes a new folder")
+        print("cd: changes directory")
+        print("ls: lists files and folders")
+        print("rmdir: removes a folder")
+        print("rmfile: removes a file")
+        print("mkfile: creates a new file")
     elif cmd  == "shutdown":
         break
     elif cmd  == "reset":
         config["isFirstTime"] = 1
         updateConfig()
+        filesys = fresh_filesys
+        updateFilesystem()
         os.system("python shell.py")
         break
     elif cmd  == "restart":
@@ -79,25 +102,41 @@ while True:
         os.system("cls")
     # make me a mkdir command
     elif cmd  == "mkdir":
-        try:
-            name = userin.split("mkdir ")[1]
-            newFolder = filesystem.Folder(name, currdir.name)
-            config = root_folder.addFolder(newFolder)
+        updateFilesystem()
+            # make a folder with the name of the userin and make the parent folder the currentdir
+        currdir.addFolder(filesystem.Folder(userin.split("mkdir ")[1], currdir.name))
+        filesys = currdir.toJson()
+        updateFilesystem()
+    # make me a removedir command
+    elif cmd  == "rmdir":
+        currdir.removeFolder(userin.split("rmdir ")[1])
+        filesys = root_folder.toJson()
+        updateFilesystem()
 
+    # make a make file command
+    elif cmd  == "mkfile":
+        try:
+            currdir.addFile(filesystem.File(userin.split("mkfile ")[1], userin.split("mkfile ")[1].split(".")[1]))
+            filesys = currdir.toJson()
             updateFilesystem()
         except:
-            pass
+            print("please specify an extension")
 
+    # make a remove file command
+    elif cmd  == "rmfile":
+        currdir.removeFile(userin.split("rmfile ")[1])
+        filesys = root_folder.toJson()
+        updateFilesystem()
     # make me a cd command
     elif cmd  == "cd":
-        try:
-            name = userin.split("cd ")[1]
-            currdir = currdir[name]
-        except:
-            if userin.split("cd ")[1] == "..":
-                currdir = currdir.parentFolder
-            else:
-                print("Invalid command.")
+        if userin.split("cd ")[1] == "..":
+            if currdir.parent != None:
+                currdir = currdir.parent
+        else:
+            try:
+                currdir = currdir.folders[userin.split("cd ")[1]]
+            except:
+                print("folder does not exist")
     elif cmd == "":
         pass
     else:
